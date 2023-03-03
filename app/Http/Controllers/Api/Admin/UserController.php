f575596c799956d895c2e29f1b82ee65ba7dc238
FabrizioCmp <fabriziocampari01@gmail.com>
2023-03-03 16:30:21 +0100

    adds userController

--- app/Http/Controllers/Api/Admin/UserController.php
+++ app/Http/Controllers/Api/Admin/UserController.php
@@ -3,9 +3,26 @@
 namespace App\Http\Controllers\Api\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $data = $request->validate(
+            [
+                "name" => "required|string",
+                "surname" => "required|string",
+                "birth_date" => "required|date",
+                "email" => "required|email",
+                "password" => "required|string",
+            ]
+        );
+
+        $user = User::create($data);
+
+        
+    }
+
 }
