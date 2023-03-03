9da9097b3a76a3f1ca20d88369db7ef2d7f02832
ippoRulez <ippolitodonato@gmail.com>
2023-03-03 12:51:31 +0100

    added messages controller with routes

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -10,17 +10,14 @@ class Message extends Model
     use HasFactory;
     protected $fillable = [
         'apartment_id',
-        'apartment_id',
-        'id',
-        'apartments',
         'sender',
-        'guest user',
         'email',
         'subject',
         'message',
     ];
 
-    public function apartment(){
-    return $this->belongsTo(Apartment::class);
+    public function apartment()
+    {
+        return $this->belongsTo(Apartment::class);
     }
 }
