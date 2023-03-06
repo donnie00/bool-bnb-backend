df8ad785beab97d9b1707a3f79aef2bacb51cae9
KellerMarika <marikakeller92@gmail.com>
2023-03-06 12:00:50 +0100

    view?

--- resources/views/structure/headerNav.blade.php
+++ resources/views/structure/headerNav.blade.php
@@ -1,23 +1,10 @@
-<nav class="navbar navbar-expand-md navbar-secondary bg-secondary shadow-sm  p-0">
+<nav class="navbar navbar-expand-md navbar-light shadow-sm  p-0">
  <div class="container-fluid px-md-5 d-flex align-items-md-end">
      <a class="navbar-brand d-flex align-items-center" href="{{ url('/') }}">
 
          <div class="logo pt-2 {{-- all'espansione del burger deve allargarsi e avvicinarsi alle voci del menu --}}">
              {{-- Keller-logo --}}
-             <svg class="keller-logo" width="112" height="111" viewBox="0 0 1426 1419" fill="none"
-                 xmlns="http://www.w3.org/2000/svg">
-                 <ellipse cx="712.84" cy="709.469" rx="393.5" ry="368" fill="" />
-                 <path d="M964.462 946.311L834.962 769.311L896.462 709.311L1069.46 946.311H964.462Z" fill="#D9D9D9"
-                     stroke="black" />
-                 <path d="M637.84 818.469L589.84 942.469L395.34 565.469H504.84L637.84 818.469Z" fill="#D9D9D9"
-                     stroke="black" />
-                 <path
-                     d="M502.34 946.469H331.34C349.007 936.302 390.74 900.269 416.34 837.469C440.74 897.869 483.84 935.302 502.34 946.469Z"
-                     fill="#D9D9D9" stroke="black" />
-                 <path d="M1046.46 563.811L948.462 660.311C941.662 603.911 902.296 572.478 883.462 563.811H1046.46Z"
-                     fill="#D9D9D9" stroke="black" />
-                 <rect x="713.841" y="167.969" width="66" height="1073" fill="#D9D9D9" stroke="black" />
-             </svg>
+            <img src="" alt="LOGO">
          </div>
          {{-- config('app.name', 'Laravel') --}}
      </a>
