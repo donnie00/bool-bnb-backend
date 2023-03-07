dbbcc32af16b165b05c1eb3228a27be23748a010
LucaConiglio <lucaconiglio.92@gmail.com>
2023-03-07 16:14:57 +0100

    add seeder

---
+++ database/seeders/Apartment_ServiceTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class Apartment_ServiceTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
