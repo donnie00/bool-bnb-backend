c84c772c2dd061c6dffdb5eba1751e6bbae861ec
FabrizioCmp <fabriziocampari01@gmail.com>
2023-03-08 12:17:38 +0100

    adds aprtmetn_service seeder

--- database/seeders/Apartment_ServiceTableSeeder.php
+++ database/seeders/Apartment_ServiceTableSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\ApartmentService;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class Apartment_ServiceTableSeeder extends Seeder
 {
@@ -12,6 +14,22 @@ class Apartment_ServiceTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $services = DB::table("services")->select("id")->pluck("id")->toArray();
+
+        $apartments_id = DB::table("apartments")->select("id")->pluck("id")->toArray();
+
+        foreach ($apartments_id as $apartment_id) {
+
+            for ($i=0; $i < 4 ; $i++) { 
+                
+                $rand_service = array_rand($services);
+
+                $apartment_service = new ApartmentService();
+                $apartment_service->apartment_id = $apartment_id;
+                $apartment_service->service_id = $services[$rand_service];
+
+                $apartment_service->save();
+            }
+        }
     }
 }
