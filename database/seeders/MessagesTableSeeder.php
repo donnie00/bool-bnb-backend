19fd7acc4f7479f1103105aa67d0f2c1f128ed19
maripiero23 <mari.pieropan@gmail.com>
2023-03-08 12:40:09 +0100

    MessageTableSeeder done

--- database/seeders/MessagesTableSeeder.php
+++ database/seeders/MessagesTableSeeder.php
@@ -2,16 +2,40 @@
 
 namespace Database\Seeders;
 
+use App\Models\Message;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Facades\DB;
 
 class MessagesTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
-    {
-        //
+    public function run(Faker $faker): void
+    {   
+        $apartments_id=DB::table("apartments")->select("id")->pluck("id")->toArray();
+
+        for ($i=0; $i <200 ; $i++) { 
+            $rand_key = array_rand($apartments_id, 1);
+            $apartment_id = $apartments_id[$rand_key];
+
+            $message = new Message();
+    
+            $message->message=$faker->text(300);
+            $message->email=$faker->email();
+            $message->sender=$faker->name();
+            $message->subject=$faker->word(1, true);
+
+            $message->apartment_id = $apartment_id;
+
+            $message->save();
+        }
+
+
+
     }
+
+    
 }
