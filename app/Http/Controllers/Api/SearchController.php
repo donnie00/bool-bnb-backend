4bacbc941f658d638a5e4f20363c5ebeef934480
ippoRulez <ippolitodonato@gmail.com>
2023-03-09 20:59:03 +0100

    added advanced search

--- app/Http/Controllers/Api/SearchController.php
+++ app/Http/Controllers/Api/SearchController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Apartment;
+use App\Models\ApartmentService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SearchController extends Controller
 {
@@ -80,14 +82,16 @@ class SearchController extends Controller
     public function search(Request $request)
     {
 
-        $data = $request->all();
-
-        // dati che ricevo dalla chiamata
         // $data = [
         //     'lat' => 41.90793,
         //     'lon' => 12.46557,
-        //     'radius' => 1.8
+        //     'radius' => 1.6910459936511,
+        //     'min_rooms' => 2,
+        //     'min_beds' => 3,
+        //     'services' => [1, 4],
         // ];
+
+        $data = $request->all();
 
         // Punto di partenza
         $searchCoord = [
@@ -116,16 +120,75 @@ class SearchController extends Controller
             $distances[$key] = $distance;
         }
 
+        asort($distances);
+
         $nearby = [];
 
         foreach ($distances as $key => $distance) {
+
+            // if ($distance <= $data['radius']) {
+            //     dump($distance);
+            //     dump($data['radius']);
+            //     dump('true');
+            //     array_push($nearby, $key);
+            // } else {
+            //     dump($distance);
+            //     dump($data['radius']);
+            //     dump('false');
+            // }
 
             if ($distance <= $data['radius']) {
                 array_push($nearby, $key);
             }
         }
 
-        $nearApartments = Apartment::whereIn('id', $nearby)->paginate(10);
+        // dump($nearby);
+
+        $apartments = [];
+
+        if ($data['min_rooms'] > 0) {
+
+            $dbApartments = DB::table("apartments")->select("id")
+                ->whereIn("id", $nearby)
+                ->where('rooms_qty', '>', $data['min_rooms'])
+                ->get()
+                ->pluck('id')
+                ->toArray();
+
+
+            $apartments = $dbApartments;
+        }
+
+
+        if ($data['min_beds'] > 0) {
+
+            $dbApartments = Apartment::select("id")->whereIn('id', $nearby)
+                ->where('beds_qty', '>', $data['min_beds'])
+                ->get()
+                ->pluck('id')
+                ->toArray();
+
+            foreach ($apartments as $key => $apartment) {
+                if (!in_array($apartment, $dbApartments)) {
+                    array_splice($apartments, $key, 1);
+                }
+            }
+        }
+
+        if (count($data['services']) > 0) {
+            foreach ($data['services'] as $service) {
+                $dbApartments = ApartmentService::select('apartment_id')->whereIn('apartment_id', $nearby)->where('service_id', $service)->get()->pluck('apartment_id')->toArray();
+                array_merge($apartments, $dbApartments);
+
+                foreach ($apartments as $key => $apartment) {
+                    if (!in_array($apartment, $dbApartments)) {
+                        array_splice($apartments, $key, 1);
+                    }
+                }
+            }
+        }
+
+        $nearApartments = Apartment::whereIn('id', $apartments)->paginate(10);
 
         return response()->json($nearApartments);
     }
