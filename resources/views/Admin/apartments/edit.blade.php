870652b1e99a2754a8ea31bb0b81fca472468fac
ippoRulez <ippolitodonato@gmail.com>
2023-03-11 11:12:11 +0100

    removed a comma

--- resources/views/Admin/apartments/edit.blade.php
+++ resources/views/Admin/apartments/edit.blade.php
@@ -276,7 +276,7 @@
    <!-- Laravel Javascript Validation -->
    <script type="text/javascript" src="{{ asset('vendor/jsvalidation/js/jsvalidation.js') }}"></script>
 
-   {!! JsValidator::formRequest('App\Http\Requests\Admin\UpdateApartmentRequest') !!};
+   {!! JsValidator::formRequest('App\Http\Requests\Admin\UpdateApartmentRequest') !!}
    <style>
       span {
          color: red
