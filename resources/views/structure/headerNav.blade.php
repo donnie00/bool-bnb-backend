76981a92299ff11c1915342f5980d4684f912f10
FabrizioCmp <fabriziocampari01@gmail.com>
2023-03-11 11:40:42 +0100

    adds new route links

--- resources/views/structure/headerNav.blade.php
+++ resources/views/structure/headerNav.blade.php
@@ -28,7 +28,7 @@
                     <li class="nav-item d-flex flex-column align-items-center">
                         <a class="nav-link fs-5  pt-md-4 px-2     
                     {{ Route::currentRouteName() === null ? 'active' : '' }}"
-                            {{--      href="{{ url('/') }}" --}} href="{{ $frontendURL }}">
+                            {{--      href="{{ url('/') }}" --}} href="{{ env('MY_FRONTEND_URL') }}">
                             {{ __('Home') }}</a>
                         <div class="link-underline m-3 mb-0"></div>
                     </li>
@@ -40,12 +40,12 @@
                     </li>
 
                     <li class="nav-item  d-flex flex-column align-items-center">
-                        <a class="nav-link fs-5  pt-md-4 px-2" href="{{ $frontendURL }}/abaut">
+                        <a class="nav-link fs-5  pt-md-4 px-2" href="{{ env('MY_FRONTEND_URL') }}/abaut">
                             {{ __('About') }}</a>
                         <div class="link-underline m-3 mb-0"></div>
                     </li>
                     <li class="nav-item  d-flex flex-column align-items-center">
-                        <a class="nav-link fs-5  pt-md-4 px-2" href="{{ $frontendURL }}/contact">
+                        <a class="nav-link fs-5  pt-md-4 px-2" href="{{ env('MY_FRONTEND_URL') }}/contact">
                             {{ __('Contacts') }}</a>
                         <div class="link-underline m-3 mb-0"></div>
                     </li>
