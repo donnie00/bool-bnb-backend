d4859b14c065cf1b731b6630444a23bbe696dc11
ippoRulez <ippolitodonato@gmail.com>
2023-03-14 17:08:47 +0100

    fixed header nav link with env frontend variable

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,38 +2,38 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+   <!-- CSRF Token -->
+   <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+   <title>{{ config('app.name', 'Laravel') }}</title>
 
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+   <!-- Fonts -->
+   <link rel="dns-prefetch" href="//fonts.gstatic.com">
+   <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
-    <!-- Usando Vite -->
-    @vite(['resources/js/app.js'])
+   <!-- Usando Vite -->
+   @vite(['resources/js/app.js'])
 </head>
 
 <body>
-    <div id="app">
-        @php
-            $frontendURL = 'http://localhost:5174';
-        @endphp
+   <div id="app">
+      @php
+         $frontendURL = env('MY_FRONTEND_URL');
+      @endphp
 
 
-        {{-- MAVBAR --}}
-        @include('structure.headerNav')
+      {{-- MAVBAR --}}
+      @include('structure.headerNav')
 
-        <main class="">
-            @yield('content')
-        </main>
+      <main class="">
+         @yield('content')
+      </main>
 
-    </div>
+   </div>
 </body>
 
 </html>
