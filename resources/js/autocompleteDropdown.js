2d6105f138b19fce9fa696d89e76bbf680d0d4d3
KellerMarika <marikakeller92@gmail.com>
2023-03-16 23:08:33 +0100

    f5 show con coso delle date

--- resources/js/autocompleteDropdown.js
+++ resources/js/autocompleteDropdown.js
@@ -1,6 +1,6 @@
 import './bootstrap';
 import '~resources/scss/app.scss';
-import * as bootstrap from 'bootstrap';
+/* import * as bootstrap from 'bootstrap'; */
 import axios from 'axios';
 import.meta.glob([
     '../img/**'
