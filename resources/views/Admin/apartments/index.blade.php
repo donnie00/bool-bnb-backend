a94029565c55a50de469ec18ad6dddb72b7471ec
FabrizioCmp <fabriziocampari01@gmail.com>
2023-03-20 15:40:11 +0100

    adds index responsive

--- resources/views/Admin/apartments/index.blade.php
+++ resources/views/Admin/apartments/index.blade.php
@@ -1,65 +1,146 @@
 @extends('layouts.dashboard')
 
 @section('content')
-   @vite(['resources/js/deleteForm.js'])
-   {{-- pezza perchè non lo recupera da dashboard layout?? --}}
-   @php
-      $frontendURL = env('MY_FRONTEND_URL');
-   @endphp
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        @media (max-width: 767px) {
+            .table {
+                display: none;
+            }
 
-   <div class="container" style="margin-left: 20px">
-      <a class="btn btn-warning my-3" href="{{ route('Admin.apartments.create') }}">Aggiungi nuovo Appartamento</a>
-      <table class="table">
-         <thead>
-            <tr>
-               <th>Titolo</th>
-               <th>Immagine</th>
-               <th>Indirizzo</th>
-               <th>Opzioni</th>
+            .layout {}
+        }
 
-            </tr>
-         </thead>
-         <tbody>
-            @foreach ($apartments as $apartment)
-               <tr>
-                  <td>{{ $apartment->title }}</td>
-                  @if ($apartment->cover_img !== null)
-                     <td><img src="{{ asset('storage/' . $apartment->cover_img) }}"
-                           style="width: 80px; height: 50px; object-fit:cover; object-position: top  " alt="Card image cap">
-                     </td>
-                  @elseif(count($apartment->images))
-                     <td><img src="{{ asset($apartment->images[0]->image) }}"
-                           style="width: 80px; height: 50px; object-fit:cover; object-position: top  " alt="Card image cap">
-                     </td>
-                  @else
-                     <td><img src="{{ asset('storage/apartments_images/placeholder-image.png') }}"
-                           style="width: 80px; height: 50px; object-fit:cover; object-position: top  "
-                           alt="Card image cap">
-                     </td>
-                  @endif
-                  <td>{{ $apartment->address }}</td>
+        @media (min-width: 768px) {
+            .small {
+                display: none;
+                list-style: none;
+            }
 
-                  <td>
-                     <div class="d-flex gap-lg-3 gap-md-2  gap-1 ">
-                        <div>
-                           <a href="{{ route('Admin.apartments.edit', $apartment->id) }}" class=" btn btn-success"><i class="fa-solid fa-pen"></i></a>
+        }
+    </style>
+    @vite(['resources/js/deleteForm.js'])
+    {{-- pezza perchè non lo recupera da dashboard layout?? --}}
+    @php
+        $frontendURL = env('MY_FRONTEND_URL');
+    @endphp
+
+    <div class="container" style="margin-left: 20px">
+        <a class="btn btn-warning my-3" href="{{ route('Admin.apartments.create') }}">Aggiungi nuovo Appartamento</a>
+        <div class="small">
+            <ul style="margin-left: -20px" class="p-0">
+                @foreach ($apartments as $apartment)
+                    <li style="list-style: none;">
+                        <div class="row">
+                            <div class="col-6">
+                                @if ($apartment->cover_img !== null)
+                                    <img src="{{ asset('storage/' . $apartment->cover_img) }}"
+                                        style="width: 80px; height: 50px; object-fit:cover; object-position: top  "
+                                        alt="Card image cap" class=" d-inline-block m-auto">
+                                @elseif(count($apartment->images))
+                                    <img src="{{ asset($apartment->images[0]->image) }}"
+                                        style="width: 80px; height: 50px; object-fit:cover; object-position: top  "
+                                        alt="Card image cap" class="m-auto">
+                                @else
+                                    <img src="{{ asset('storage/apartments_images/placeholder-image.png') }}"
+                                        style="width: 80px; height: 50px; object-fit:cover; object-position: top  "
+                                        alt="Card image cap">
+                                @endif
+                            </div>
+                            <div class=" col-6 gap-1 ">
+                                <div class="row">
+                                    <div class="p-0 col-6">
+                                        <a href="{{ route('Admin.apartments.edit', $apartment->id) }}"
+                                            class="m-auto w-50 h-75 btn "><i class="w-75 fa-solid fa-pen "></i></a>
+                                    </div>
+                                    <div class="col-6 p-0 ">
+                                        {{-- {{ route('Admin.apartments.show', $apartment->id) }} --}}
+                                        <a href="{{ route('Admin.apartments.show', $apartment->id) }}"
+                                            class="h-75 text-center  p-1 btn "><i class="fa-solid fa-eye"></i></a>
+                                    </div>
+
+
+                                    <form class="delete-form col-6 p-0 m-0 "
+                                        action="{{ route('Admin.apartments.destroy', $apartment->id) }}" method="POST">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button class="h-75 p-1 m-auto destroy-btn btn  text-dark"><i
+                                                class="ms-1 fa-regular fa-trash-can"></i></button>
+                                    </form>
+
+                                    <div class="col-6 p-0">
+                                        <a href="{{ route('subs.form', $apartment->id) }}"
+                                            class="h-75   ms-lg-3  btn  rounded-4 px-2 px-lg-4 "><i
+                                                class="fa-brands fa-shopify"></i></a>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div>
-                           {{-- {{ route('Admin.apartments.show', $apartment->id) }} --}}
-                           <a href="{{ route('Admin.apartments.show', $apartment->id) }}" class=" btn btn-warning"><i class="fa-solid fa-eye"></i></a>
-                        </div>
+
+                    </li>
+                    <hr class="m-1">
+                @endforeach
+
+            </ul>
+        </div>
+        <table class="table L">
+            <thead>
+                <tr>
+                    <th>Titolo</th>
+                    <th>Immagine</th>
+                    <th>Indirizzo</th>
+                    <th>Opzioni</th>
+
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($apartments as $apartment)
+                    <tr>
+                        <td>{{ $apartment->title }}</td>
+                        @if ($apartment->cover_img !== null)
+                            <td><img src="{{ asset('storage/' . $apartment->cover_img) }}"
+                                    style="width: 80px; height: 50px; object-fit:cover; object-position: top  "
+                                    alt="Card image cap">
+                            </td>
+                        @elseif(count($apartment->images))
+                            <td><img src="{{ asset($apartment->images[0]->image) }}"
+                                    style="width: 80px; height: 50px; object-fit:cover; object-position: top  "
+                                    alt="Card image cap">
+                            </td>
+                        @else
+                            <td><img src="{{ asset('storage/apartments_images/placeholder-image.png') }}"
+                                    style="width: 80px; height: 50px; object-fit:cover; object-position: top  "
+                                    alt="Card image cap">
+                            </td>
+                        @endif
+                        <td>{{ $apartment->address }}</td>
+
+                        <td>
+                            <div class="d-flex gap-lg-3 gap-md-2  gap-1 ">
+                                <div>
+                                    <a href="{{ route('Admin.apartments.edit', $apartment->id) }}"
+                                        class=" btn btn-success"><i class="fa-solid fa-pen"></i></a>
+                                </div>
+                                <div>
+                                    {{-- {{ route('Admin.apartments.show', $apartment->id) }} --}}
+                                    <a href="{{ route('Admin.apartments.show', $apartment->id) }}"
+                                        class=" btn btn-warning"><i class="fa-solid fa-eye"></i></a>
+                                </div>
 
 
-                        <form class="delete-form " action="{{ route('Admin.apartments.destroy', $apartment->id) }}"
-                           method="POST">
-                           @csrf
-                           @method('DELETE')
-                           <button class="destroy-btn btn btn-danger text-dark"><i class="fa-regular fa-trash-can"></i></button>
-                        </form>
+                                <form class="delete-form " action="{{ route('Admin.apartments.destroy', $apartment->id) }}"
+                                    method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button class="destroy-btn btn btn-danger text-dark"><i
+                                            class="fa-regular fa-trash-can"></i></button>
+                                </form>
 
-                           <div>
-                               <a href="{{route("subs.form", $apartment->id)}}" class=" ms-2  ms-lg-3  btn btn-info rounded-4 px-2 px-lg-4 "><i class="fa-brands fa-shopify"></i></a>
-                           </div>
+                                <div>
+                                    <a href="{{ route('subs.form', $apartment->id) }}"
+                                        class=" ms-2  ms-lg-3  btn btn-info rounded-4 px-2 px-lg-4 "><i
+                                            class="fa-brands fa-shopify"></i></a>
+                                </div>
 
                             </div>
                         </td>
@@ -70,4 +151,3 @@
         </table>
     </div>
 @endsection
-
